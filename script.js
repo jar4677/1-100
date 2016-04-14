4e06ac70a5076c3dd6e708deb7c54cc1f7863133
Jon Rasmussen <jonathan.a.rasmussen@gmail.com>
2016-04-14 11:34:25 -0700

    changed to switch/case

--- script.js
+++ script.js
@@ -16,34 +16,23 @@ $("document").ready(function () {
 function makeGuess() {
     var theGuess = $("#guess_input").val();
 
-    if (theGuess > theNumber){
-        $("#response").html("Too High!");
-        $("#guess").css('background-color', 'red');
-    } else if (theGuess < theNumber){
-        $("#response").html("Too Low!");
-        $("#guess").css('background-color', 'blue');
-    } else {
-        $("#response").html("You guessed it!");
-        $("#guess").css('background-color', 'green');
+    switch (true){
+        case (isNaN(theGuess) || theGuess < 1 || theGuess > 10):
+            $("#response").html("Invalid Input");
+            break;
+        case (theGuess > theNumber):
+            $("#response").html("Too High!");
+            $("#guess").css('background-color', 'red');
+            break;
+        case (theGuess < theNumber):
+            $("#response").html("Too Low!");
+            $("#guess").css('background-color', 'blue');
+            break;
+        default :
+            $("#response").html("You guessed it!");
+            $("#guess").css('background-color', 'green');
+            break;
     }
-
-    // switch (true){
-    //     case (isNaN(theGuess) || theGuess < 1 || theGuess > 10):
-    //         $("#response").html("Invalid Input");
-    //         break;
-    //     case (theGuess > theNumber):
-    //         $("#response").html("Too High!");
-    //         $("#guess").css('background-color', 'red');
-    //         break;
-    //     case (theGuess < theNumber):
-    //         $("#response").html("Too Low!");
-    //         $("#guess").css('background-color', 'blue');
-    //         break;
-    //     default :
-    //         $("#response").html("You guessed it!");
-    //         $("#guess").css('background-color', 'green');
-    //         break;
-    // }
 }
 
 $("#guess").click(function () {
