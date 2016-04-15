f92bfecd41f038c97aba362ed1c02cff3039675e
Jon Rasmussen <jonathan.a.rasmussen@gmail.com>
2016-04-15 09:40:50 -0700

    color scaling complete

--- script.js
+++ script.js
@@ -8,22 +8,19 @@ var correct = false;
 var score = 101;
 
 //color change variables
-var hiRange;
-var loRange;
-var guessDif;
-var rgb = [255, 0, 255];
-var color = "rgb(" + rgb.join(", ") + ")";
+var low;
+var high;
+var color;
 
 
 //pick a random number
 function pickNumber() {
     var randomNumber = (Math.ceil(Math.random() * 100));
-    loRange = (randomNumber - 1);
-    hiRange = (100 - (randomNumber + 1));
+    low = ((randomNumber - 1)/2);
+    high = (randomNumber + ((100 - (randomNumber + 1))/2));
     console.log('random #: ', randomNumber);
-    // console.log('loRange : ', loRange);
-    // console.log('hiRange : ', hiRange);
-    console.log('rgb_1: ', rgb);
+    console.log('low : ', low);
+    console.log('high : ', high);
     return (randomNumber);
 }
 
@@ -35,22 +32,32 @@ $("document").ready(function () {
 //submit guess
 function makeGuess() {
     var theGuess = $("#guess_input").val();
-    guessDif = (Math.abs(theNumber - theGuess) - 1);
 
     //set rgb values by %
-    function setColor(loOrhi) {
-        var x = ((loOrhi - guessDif) / loOrhi);
-        console.log('dif %: ', x);
-        if (x < .5){
-            rgb[0] = Math.round(255 * x);
-            rgb[2] = 255;
-        } else if (x > .5){
-            rgb[2] = Math.round(255 * x);
-            rgb[0] = 255;
+    function setColor(highLow) {
+        //gets the absolute value of the difference from the median
+        var guessDif = Math.abs(highLow - theGuess);
+
+        //gets the rgb value depending on whether the guess is high or low
+        if (theGuess < theNumber) {
+            var x = Math.round(255 * ((highLow - guessDif) / highLow));
+            console.log('rgb value: ', x);
+        } else {
+            var y = 100 - highLow;
+            var x = Math.round(255 * ((y - guessDif) / y));
         }
-        color = "rgb(" + rgb.join(", ") + ")";
+
+        //sets the rgb value to the color variable
+        if (theGuess > low && theGuess < high){
+            // color = "rgb(255," + x + ", " + x + ")";
+            color = "rgb(255, 0, " + x + ")";
+        } else {
+            // color = "rgb(" + x + ", " + x + ", 255)";
+            color = "rgb(" + x + ", 0, 255)";
+        }
     }
 
+    //makes changes based on input
     switch (true){
         case (isNaN(theGuess) || theGuess < 1 || theGuess > 100):
             $("#response").html("Invalid Input");
@@ -60,16 +67,16 @@ function makeGuess() {
         case (theGuess > theNumber):
             $("#response").html("Too High!");
             $("#reprompt").css('display', 'block');
-            setColor(hiRange);
-            console.log('rgb: ', rgb, 'color: ', color);
+            setColor(high);
+            console.log('color: ', color);
             $("#guess").css('background-color', color);
             $("#guess_input").focus();
             break;
         case (theGuess < theNumber):
             $("#response").html("Too Low!");
             $("#reprompt").css('display', 'block');
-            setColor(loRange);
-            console.log('rgb: ', rgb, 'color: ', color);
+            setColor(low);
+            console.log('color: ', color);
             $("#guess").css('background-color', color);
             $("#guess_input").focus();
             break;
