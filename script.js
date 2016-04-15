ba0368ead0cdd2a58c4c577d8df851ed8fd28fca
Jon Rasmussen <jonathan.a.rasmussen@gmail.com>
2016-04-14 18:44:31 -0700

    so closegit add .

--- script.js
+++ script.js
@@ -7,18 +7,49 @@ var theNumber = null;
 var correct = false;
 var score = 101;
 
+//color change variables
+var hiRange;
+var loRange;
+var guessDif;
+var rgb = [255, 0, 255];
+var color = "rgb(" + rgb.join(", ") + ")";
+
+
+//pick a random number
 function pickNumber() {
-    return (Math.ceil(Math.random() * 100));
+    var randomNumber = (Math.ceil(Math.random() * 100));
+    loRange = (randomNumber - 1);
+    hiRange = (100 - (randomNumber + 1));
+    console.log('random #: ', randomNumber);
+    // console.log('loRange : ', loRange);
+    // console.log('hiRange : ', hiRange);
+    console.log('rgb_1: ', rgb);
+    return (randomNumber);
 }
 
+//run the first time
 $("document").ready(function () {
-    theNumber = pickNumber()
-    console.log('load #:',theNumber);
+    theNumber = pickNumber();
 });
 
 //submit guess
 function makeGuess() {
     var theGuess = $("#guess_input").val();
+    guessDif = (Math.abs(theNumber - theGuess) - 1);
+
+    //set rgb values by %
+    function setColor(loOrhi) {
+        var x = ((loOrhi - guessDif) / loOrhi);
+        console.log('dif %: ', x);
+        if (x < .5){
+            rgb[0] = Math.round(255 * x);
+            rgb[2] = 255;
+        } else if (x > .5){
+            rgb[2] = Math.round(255 * x);
+            rgb[0] = 255;
+        }
+        color = "rgb(" + rgb.join(", ") + ")";
+    }
 
     switch (true){
         case (isNaN(theGuess) || theGuess < 1 || theGuess > 100):
@@ -29,13 +60,17 @@ function makeGuess() {
         case (theGuess > theNumber):
             $("#response").html("Too High!");
             $("#reprompt").css('display', 'block');
-            $("#guess").css('background-color', 'red');
+            setColor(hiRange);
+            console.log('rgb: ', rgb, 'color: ', color);
+            $("#guess").css('background-color', color);
             $("#guess_input").focus();
             break;
         case (theGuess < theNumber):
             $("#response").html("Too Low!");
             $("#reprompt").css('display', 'block');
-            $("#guess").css('background-color', 'blue');
+            setColor(loRange);
+            console.log('rgb: ', rgb, 'color: ', color);
+            $("#guess").css('background-color', color);
             $("#guess_input").focus();
             break;
         default :
@@ -49,6 +84,7 @@ function makeGuess() {
     }
 }
 
+//reset game
 function reset() {
     theNumber = pickNumber();
     $("#response").html("Click Here");
@@ -60,6 +96,7 @@ function reset() {
     score = 101;
 }
 
+//click handler
 $("#guess").click(function () {
     if (correct) {
         reset();
